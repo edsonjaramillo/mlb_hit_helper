fb5266e716951aaaf06e7dfea251e1efeca24afc
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-07 18:30:48 -0500

    ' to &apos

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => (
   <div className='header responsive-width-header'>
-    <h1 className="header__main">Welcome to Edson's MLB Hit Helper!</h1>
+    <h1 className="header__main">Welcome to Edson&apos;s MLB Hit Helper!</h1>
     <p className="header__sub">
       This web application was built to simplify my process of finding the best current batters for
       2 to 3 bet parlays for hits.
