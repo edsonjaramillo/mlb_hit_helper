dd7e6b4d73f818c01782395f859e6aaf384ac490
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-08 14:59:40 -0500

    format

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,18 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.query.token !== process.env.REVALIDATE_TOKEN) {
-    return res.status(401).json({ message: 'Invalid token' })
+    return res.status(401).json({ message: 'Invalid token' });
   }
   try {
-    await res.unstable_revalidate('/')
-    return res.json({ revalidated: true })
+    await res.unstable_revalidate('/');
+    return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send('Error revalidating')
+    return res.status(500).send('Error revalidating');
   }
-
 }
