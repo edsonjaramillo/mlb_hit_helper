2ec9b3a8748405cb6258618829dcf24eaf65d5c4
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-27 16:38:05 -0500

    added more info to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,25 @@
-import { dateFormatter } from "@/lib/dateFormatter";
+import { dateFormatter } from '@/lib/dateFormatter';
 
 interface HeaderProps {
   publishedAt: string;
 }
 
-const Header = ({publishedAt}:HeaderProps) => (
+const Header = ({ publishedAt }: HeaderProps) => (
   <div className='header responsive-width-header'>
     <h1 className='header__main'>Welcome to Edson&apos;s MLB Hit Helper!</h1>
     <p className='header__sub'>
       This web application was built to simplify my process of finding the best current batters for
-      2 to 3 bet parlays for hits.
+      2 to 3 bet parlays for hits. Only the teams that play today are shown.
     </p>
-    <span>{`Stats gathered on  ${dateFormatter(publishedAt)} CT `}</span>
+    <strong>{`Stats gathered on  ${dateFormatter(publishedAt)} CT `}</strong>
     <div className='header__links'>
       <ExternalLink
-        href='https://github.com/edsonjaramillo/mlb_hit_helper'
+        href='https://github.com/edsonjaramillo/daily-at-bats-scraper'
         text='View Repo'
         apperanceClass='header--github'
       />
       <ExternalLink
-        href={`https://venmo.com/code?user_id=2570444435619840115&created=1651967382.92727&printed=1`}
+        href='https://account.venmo.com/u/EdsonJaramillo'
         text='Donate'
         apperanceClass='header--venmo'
       />
@@ -32,6 +32,7 @@ interface ExternalLinkProps {
   apperanceClass: string;
   text: string;
 }
+
 const ExternalLink = ({ href, apperanceClass, text }: ExternalLinkProps) => (
   <a
     className={`header__link ${apperanceClass}`}
